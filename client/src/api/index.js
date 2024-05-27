a69e2425556bd980bc511d8b730a6846d59d9b35
sanskarsri18 <99880907+sanskarsri18@users.noreply.github.com>
2024-05-28 03:05:23 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://generator-5y07.onrender.com/api/",
 });
 
 export const GetPosts = async () => await API.get("/post/");
